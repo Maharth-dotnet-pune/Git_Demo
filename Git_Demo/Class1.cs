2e70b69620333f4408cf772c8db97ab330776168
Aditya.Airan <Aditya.Airan@ENCDAPPUNLT0350>
2025-10-29 18:12:24 +0530

    this is a new branch

---
+++ Git_Demo/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Demo
+{
+    internal class Class1
+    {
+        public void DisplayMessage()
+        {
+            Console.WriteLine("Hello from Class1 in Git_Demo namespace!");
+        }
+    }
+}
