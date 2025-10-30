41d852ef67d59bdd5fdc3b5c6c45132ff826776e
dipanshu-dotnet-pune <dipanshumandal17@gmail.com>
2025-10-30 10:21:06 +0530

    class3

---
+++ Git_Demo/Class3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Demo
+{
+    internal class Class3
+    {
+        int classnumber = 3;
+    }
+}
