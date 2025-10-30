23fc33e47ea8d177f1336c6d80a8297203533516
dev-dotnet-pune <devgovani2003@gmail.com>
2025-10-30 10:14:00 +0530

    dev-pune

---
+++ Git_Demo/Class7.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Demo
+{
+    internal class Class7
+    {
+        string StudentName = "";
+    }
+}
